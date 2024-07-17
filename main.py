3a0459de1b0f69379357f0b1be37abb9198b79cf
Theo Tarr <theodore@tarr.com>
2024-07-17 14:25:08 -0400

    fetch data stream, example data visualization

---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+    Minimal usage example
+    Connects to QTM and streams 3D data forever
+    (start QTM first, load file, Play->Play with Real-Time output)
+"""
+
+import asyncio
+import qtm_rt
+
+IP_ADDRESS = "140.247.112.125"
+PASSWORD = "$KHU15"
+
+
+def on_packet(packet):
+    """ Callback function that is called everytime a data packet arrives from QTM """
+    print("Framenumber: {}".format(packet.framenumber))
+    header, markers = packet.get_3d_markers()
+    print("Component info: {}".format(header))
+    for marker in markers:
+        print("\t", marker)
+
+
+async def setup():
+    """ Main function """
+    connection = await qtm_rt.connect(IP_ADDRESS, version="1.8") # Use at least version 1.8
+    if connection is None:
+        return
+
+    await connection.stream_frames(components=["3d"], on_packet=on_packet)
+
+
+if __name__ == "__main__":
+    asyncio.ensure_future(setup())
+    asyncio.get_event_loop().run_forever()
