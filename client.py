b17f2102c08675a01bddc71775dd22010939b624
Theo Tarr <theodore@tarr.com>
2024-07-19 10:04:14 -0400

    pubsub

---
+++ client.py
@@ -0,0 +1,103 @@
+import os
+import json
+import logging
+import numpy as np
+import zmq
+
+
+from datetime import datetime
+from utils.logging import print_elapsed_time
+
+PUBLISHER_SOCKET = "tcp://127.0.0.1:5555"
+
+
+def connect_to_publisher(logger: logging.Logger = None) -> zmq.Socket:
+    """Connect to publisher socket and return subscriber socket
+
+    Args:
+        logger (logging.Logger, optional): Logger, defaults to None.
+    Returns:
+        zmq.Socket: subscriber socket
+    """
+    if logger:
+        logger.info("Connecting to publisher...")
+    context = zmq.Context()
+
+    # Set up subscriber
+    subscriber = context.socket(zmq.SUB)
+    subscriber.connect(PUBLISHER_SOCKET)
+    subscriber.setsockopt_string(zmq.SUBSCRIBE, "")
+
+    return subscriber
+
+
+def setup_client_logger() -> logging.Logger:
+    """Setup logger for client"""
+    # Generate logfile name
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    logfile = f"./logs/{current_time}_client_marker_processing.log"
+
+    os.makedirs(os.path.dirname(logfile), exist_ok=True)
+    logging.basicConfig(
+        filename=logfile, format="%(asctime)s %(message)s", filemode="w"
+    )
+
+    logger = logging.getLogger("Client logger")
+    logger.setLevel(logging.DEBUG)
+
+    return logger
+
+
+def get_qrt_data(logger: logging.Logger, socket: zmq.Socket) -> dict:
+    """Get marker and force data from Motion Capture
+
+    Args:
+        logger (logging.Logger): logger
+        socket (zmq.Socket): zmq publisher socket
+
+    Returns:
+        dict: contains organized marker and force data
+    """
+    # Retrieve data from server
+    marker_data = []
+    analog_data = []
+
+    rt_data = read_mocap_data(logger=logger, socket=socket)
+
+    # Measure time to build dicts
+    for rt_id, data in rt_data.items():
+        if "marker" in rt_id:
+            marker_data.append(np.array(data))
+        elif "analog" in rt_id:
+            analog_data(np.array(data))
+
+    # logger.info(f"timestamp: \n{time.time()}")
+    # logger.info(f"Force data: \n{force_data}")
+    logger.info(f"Marker data: \n{marker_data}")
+    logger.info(f"Analog data: \n{analog_data}")
+
+    return marker_data, analog_data
+
+
+def read_mocap_data(logger: logging.Logger, socket: zmq.Socket) -> dict:
+    """Read mocap data from publisher node
+
+    Args:
+        logger (logging.Logger): logger
+        socket (zmq.Socket): zmq socket
+    Returns:
+        dict: contains raw data from server
+    """
+    try:
+        # Read data from publisher
+        message = socket.recv_string()
+        try:
+            rt_data = json.loads(message)
+        except json.JSONDecodeError as error:
+            logger.error(f"An error occurred while decoding JSON: {error}")
+            return rt_data
+
+    except Exception as general_error:
+        logger.error(f"An unexpected error occurred: {general_error}")
+
+    return rt_data
---
+++ server.py
@@ -0,0 +1,54 @@
+import json
+import zmq
+import asyncio
+import qtm_rt
+import numpy as np
+from utils.logging import print_elapsed_time
+
+IP_ADDRESS = "140.247.112.125"
+PASSWORD = "$KHU15"
+
+
+@print_elapsed_time()
+def on_packet(packet):
+    """Callback function that is called everytime a data packet arrives from QTM"""
+    _, markers = packet.get_3d_markers()
+
+    points = [np.array([marker.x, marker.y, marker.z]) for marker in markers]
+    print(f"Received data for frame {packet.framenumber}, {len(points)} markers")
+    # print(points)
+
+    dict_market = {"markers": points}
+
+    marker_json = json.dumps(dict_market)
+
+    # Publish message to all subscribers
+    publisher.send_string(marker_json)
+
+
+async def setup():
+    print(f"Attempting to connect to QTM at {IP_ADDRESS}")
+    try:
+        connection = await asyncio.wait_for(
+            qtm_rt.connect(IP_ADDRESS, version="1.8"), timeout=5.0
+        )
+        if connection is None:
+            print("Failed to connect to QTM")
+            return None
+        print("Connected to QTM")
+        return connection
+    except asyncio.TimeoutError:
+        print("Connection attempt timed out")
+        return None
+    except Exception as e:
+        print(f"Error connecting to QTM: {e}")
+        return None
+
+
+if __name__ == "__main__":
+    context = zmq.Context()
+    publisher = context.socket(zmq.PUB)
+    publisher.bind("tcp://*:5555")
+
+    asyncio.ensure_future(setup())
+    asyncio.get_event_loop().run_forever()
